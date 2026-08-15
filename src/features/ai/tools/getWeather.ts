bd0cda92a24663e3b34f0e45d7773f7b6aa6750b
keplar <shehabhossen76.services@gmail.com>
2026-08-15 23:33:07 +0600

    Chat api added.

--- src/features/ai/tools/getWeather.ts
+++ src/features/ai/tools/getWeather.ts
@@ -1,6 +1,7 @@
 import { tool } from "langchain";
 import * as z from "zod";
 
+// model call
 export const getWeather = tool(async ({ location }) => `Sunny in ${location}`, {
   name: "get_weather",
   description: "Get the current weather in a given location",
